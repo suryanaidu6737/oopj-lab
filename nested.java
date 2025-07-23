b4a5a426b3f2b2b0be747bdd3c42971152bb5070
suryanaidu6737 <yerramsettisurya445@gmail.com>
2025-07-23 12:34:57 +0530

    Update nested.java

--- nested.java
+++ nested.java
@@ -1,39 +1,38 @@
 import java.util.Scanner;
 class nestedif
 {
-public static void main(String[] args)
-{
-	Scanner x=new Scanner(System.in);
-	System.out.println("enter a");
-	int a=x.nextInt();
-	System.out.println("enter b");
-	int b=x.nextInt();
-	System.out.println("enter c");
-	int c=x.nextInt();
-	if(a<b)
+	public static void main(String[] args)
 	{
-		if(b<c)
+		Scanner x=new Scanner(System.in);
+		System.out.println("enter a");
+		int a=x.nextInt();
+		System.out.println("enter b");
+		int b=x.nextInt();
+		System.out.println("enter c");
+		int c=x.nextInt();
+		if(a<b)
 		{
-			System.out.println("c is greatest");
+			if(b<c)
+			{
+				System.out.println("c is greatest");
+			}
+			else
+			{
+				System.out.println("b is greatest");
+			}	
 		}
 		else
 		{
-			System.out.println("b is greatest");
+			if(a<c)
+			{
+				System.out.println("c is greatest");
+			}
+			else
+			{
+				System.out.println("a is greatest");
+			}
 		}
-		
+		x.close();
 	}
-	else
-	{
-		if(a<c)
-		{
-			System.out.println("c is greatest");
-		}
-		else
-		{
-			System.out.println("a is greatest");
-		}
-	}
-	x.close();
-}
 }
 
