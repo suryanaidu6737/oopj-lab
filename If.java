c9a3755d2c0949eee45304bf5810eaac6c1e0ab0
suryanaidu6737 <yerramsettisurya445@gmail.com>
2025-07-23 12:32:32 +0530

    Update If.java

--- If.java
+++ If.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 class If
 {
-public static void main(String[] args)
-{
-	Scanner x=new Scanner(System.in);
-	System.out.println("enter age");
-	int age=x.nextInt();
-	if(age==18)
+	public static void main(String[] args)
 	{
-		System.out.println(age+"eligible to vote");
+		Scanner x=new Scanner(System.in);
+		System.out.println("enter age");
+		int age=x.nextInt();
+		if(age==18)
+		{
+			System.out.println(age+"eligible to vote");
+		}
 	}
-}
 }
